ad4aa7002e21ab6d9acedb4bb836960eccf6a72c
somil kumar <somil.kumar91@gmail.com>
2017-11-11 22:31:33 +0530

    Delete query : many , one & findOneAndDelete

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,39 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID,  } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err) {
+        return console.log('Unable to connect to mongo db server');
+    } 
+    
+    console.log('Connected to MongoDB server');
+    /**
+     * Delete Query 
+     */
+
+
+
+    /**
+     * deleteMany
+    */
+
+    // db.collection('Todos').deleteMany({text: 'Test todo'}).then((result) => {
+    //     console.log(result);
+    // })
+
+    /**
+     * deleteOne
+    */
+
+    // db.collection('Todos').deleteOne({text: 'Test todo'}).then((result) => {
+    //     console.log(result);
+    // })
+
+    /**
+     * findOneAndDelete
+    */
+
+    db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+        console.log(result);
+    })
+});
--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -19,8 +19,9 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //     console.log('Unable to fetch todos', err);
     // })
 
-    // Count Query
-
+    /**
+     * Count Query
+     */
     // db.collection('Todos').find().count().then((count) => {
     //     console.log(`Todos count : ${count}`);
     // }, (err) => {
