143edab73e654d42204b73fab6ca7d8677ab979c
somil kumar <somil.kumar91@gmail.com>
2017-12-13 21:03:38 +0530

    Adding a todo Post api

--- server/server.js
+++ server/server.js
@@ -1,60 +1,52 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise  = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+        }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
-// var newTodo = new Todo({
-//     text: 'Cook Dinner'
+
+app.listen(3000, ()=> {
+    console.log('Started on port 3000');
+});
+
+
+
+// var otherTodo = new Todo({
+//     text: 'Play',
 // });
 
-// newTodo.save().then(todoSaveSuccess, todoSaveError);
-
-var otherTodo = new Todo({
-    text: 'Play',
-});
-
-otherTodo.save().then(saveSuccess, saveError);
+// otherTodo.save().then(saveSuccess, saveError);
 
 
+// var newUser = new User({
+//     email: 'xyz@qer.com',
+// });
 
-
-
-var User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    }
-});
-
-var newUser = new User({
-    email: 'xyz@qer.com',
-});
-
-newUser.save().then(saveSuccess, saveError);
+// newUser.save().then(saveSuccess, saveError);
 
 
 
 function saveSuccess(doc) {
-    console.log(JSON.stringify(doc, undefined, 2))
+    console.log(JSON.stringify(doc, undefined, 2));
+    res
 }
 
 function saveError(e) {
